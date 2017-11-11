637d53c06c2529f849190d66887b4aca6651f5e6
Anthony Green <green.an@husky.neu.edu>
2017-11-11 16:04:39 -0500

    put states in the repo in prive/repo/seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias WhatsHoppin.Locations.State
+alias WhatsHoppin.Repo
+
+Repo.delete_all(State)
+Application.get_env(:whats_hoppin, :states)
+|> Enum.map(fn (s) ->
+	Repo.insert!(%State{name: s}) 
+end)
