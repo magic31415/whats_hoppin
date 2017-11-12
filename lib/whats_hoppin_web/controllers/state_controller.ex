67feb6bd4c8bdc61d5631128e76c27622cc65253
Max Corwin <maxcorwin@yahoo.com>
2017-11-12 18:20:50 -0500

    cleanup: Commented out unnecessary controller actions

--- lib/whats_hoppin_web/controllers/state_controller.ex
+++ lib/whats_hoppin_web/controllers/state_controller.ex
@@ -9,21 +9,21 @@ defmodule WhatsHoppinWeb.StateController do
     render(conn, "index.html", states: states)
   end
 
-  def new(conn, _params) do
-    changeset = Locations.change_state(%State{})
-    render(conn, "new.html", changeset: changeset)
-  end
+  # def new(conn, _params) do
+  #   changeset = Locations.change_state(%State{})
+  #   render(conn, "new.html", changeset: changeset)
+  # end
 
-  def create(conn, %{"state" => state_params}) do
-    case Locations.create_state(state_params) do
-      {:ok, state} ->
-        conn
-        |> put_flash(:info, "State created successfully.")
-        |> redirect(to: state_path(conn, :show, state))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
+  # def create(conn, %{"state" => state_params}) do
+  #   case Locations.create_state(state_params) do
+  #     {:ok, state} ->
+  #       conn
+  #       |> put_flash(:info, "State created successfully.")
+  #       |> redirect(to: state_path(conn, :show, state))
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "new.html", changeset: changeset)
+  #   end
+  # end
 
   def show(conn, %{"id" => id}) do
     state = Locations.get_state!(id)
@@ -31,31 +31,31 @@ defmodule WhatsHoppinWeb.StateController do
     render(conn, "show.html", state: state, breweries_in_state: breweries)
   end
 
-  def edit(conn, %{"id" => id}) do
-    state = Locations.get_state!(id)
-    changeset = Locations.change_state(state)
-    render(conn, "edit.html", state: state, changeset: changeset)
-  end
+  # def edit(conn, %{"id" => id}) do
+  #   state = Locations.get_state!(id)
+  #   changeset = Locations.change_state(state)
+  #   render(conn, "edit.html", state: state, changeset: changeset)
+  # end
 
-  def update(conn, %{"id" => id, "state" => state_params}) do
-    state = Locations.get_state!(id)
+  # def update(conn, %{"id" => id, "state" => state_params}) do
+  #   state = Locations.get_state!(id)
 
-    case Locations.update_state(state, state_params) do
-      {:ok, state} ->
-        conn
-        |> put_flash(:info, "State updated successfully.")
-        |> redirect(to: state_path(conn, :show, state))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", state: state, changeset: changeset)
-    end
-  end
+  #   case Locations.update_state(state, state_params) do
+  #     {:ok, state} ->
+  #       conn
+  #       |> put_flash(:info, "State updated successfully.")
+  #       |> redirect(to: state_path(conn, :show, state))
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "edit.html", state: state, changeset: changeset)
+  #   end
+  # end
 
-  def delete(conn, %{"id" => id}) do
-    state = Locations.get_state!(id)
-    {:ok, _state} = Locations.delete_state(state)
+  # def delete(conn, %{"id" => id}) do
+  #   state = Locations.get_state!(id)
+  #   {:ok, _state} = Locations.delete_state(state)
 
-    conn
-    |> put_flash(:info, "State deleted successfully.")
-    |> redirect(to: state_path(conn, :index))
-  end
+  #   conn
+  #   |> put_flash(:info, "State deleted successfully.")
+  #   |> redirect(to: state_path(conn, :index))
+  # end
 end
--- lib/whats_hoppin_web/controllers/style_controller.ex
+++ lib/whats_hoppin_web/controllers/style_controller.ex
@@ -9,52 +9,52 @@ defmodule WhatsHoppinWeb.StyleController do
     render(conn, "index.html", styles: styles)
   end
 
-  def new(conn, _params) do
-    changeset = Beer.change_style(%Style{})
-    render(conn, "new.html", changeset: changeset)
-  end
+  # def new(conn, _params) do
+  #   changeset = Beer.change_style(%Style{})
+  #   render(conn, "new.html", changeset: changeset)
+  # end
 
-  def create(conn, %{"style" => style_params}) do
-    case Beer.create_style(style_params) do
-      {:ok, style} ->
-        conn
-        |> put_flash(:info, "Style created successfully.")
-        |> redirect(to: style_path(conn, :show, style))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
+  # def create(conn, %{"style" => style_params}) do
+  #   case Beer.create_style(style_params) do
+  #     {:ok, style} ->
+  #       conn
+  #       |> put_flash(:info, "Style created successfully.")
+  #       |> redirect(to: style_path(conn, :show, style))
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "new.html", changeset: changeset)
+  #   end
+  # end
 
   def show(conn, %{"id" => id}) do
     style = Beer.get_style!(id)
     render(conn, "show.html", style: style)
   end
 
-  def edit(conn, %{"id" => id}) do
-    style = Beer.get_style!(id)
-    changeset = Beer.change_style(style)
-    render(conn, "edit.html", style: style, changeset: changeset)
-  end
+  # def edit(conn, %{"id" => id}) do
+  #   style = Beer.get_style!(id)
+  #   changeset = Beer.change_style(style)
+  #   render(conn, "edit.html", style: style, changeset: changeset)
+  # end
 
-  def update(conn, %{"id" => id, "style" => style_params}) do
-    style = Beer.get_style!(id)
+  # def update(conn, %{"id" => id, "style" => style_params}) do
+  #   style = Beer.get_style!(id)
 
-    case Beer.update_style(style, style_params) do
-      {:ok, style} ->
-        conn
-        |> put_flash(:info, "Style updated successfully.")
-        |> redirect(to: style_path(conn, :show, style))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", style: style, changeset: changeset)
-    end
-  end
+  #   case Beer.update_style(style, style_params) do
+  #     {:ok, style} ->
+  #       conn
+  #       |> put_flash(:info, "Style updated successfully.")
+  #       |> redirect(to: style_path(conn, :show, style))
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "edit.html", style: style, changeset: changeset)
+  #   end
+  # end
 
-  def delete(conn, %{"id" => id}) do
-    style = Beer.get_style!(id)
-    {:ok, _style} = Beer.delete_style(style)
+  # def delete(conn, %{"id" => id}) do
+  #   style = Beer.get_style!(id)
+  #   {:ok, _style} = Beer.delete_style(style)
 
-    conn
-    |> put_flash(:info, "Style deleted successfully.")
-    |> redirect(to: style_path(conn, :index))
-  end
+  #   conn
+  #   |> put_flash(:info, "Style deleted successfully.")
+  #   |> redirect(to: style_path(conn, :index))
+  # end
 end
