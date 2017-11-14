cefbffa07c4ae14b43d9eddf6d7eea9e09148d97
Max Corwin <maxcorwin@yahoo.com>
2017-11-14 01:52:17 -0500

    channels: Changed messages to take one forum_id

--- lib/whats_hoppin/forum/message.ex
+++ lib/whats_hoppin/forum/message.ex
@@ -6,9 +6,8 @@ defmodule WhatsHoppin.Forum.Message do
 
   schema "messages" do
     field :content, :string
+    field :forum_id, :string
     belongs_to :user, WhatsHoppin.Forum.User
-    belongs_to :brewery, WhatsHoppin.Locations.Brewery
-    belongs_to :style, WhatsHoppin.Beer.Style
 
     timestamps()
   end
@@ -16,33 +15,7 @@ defmodule WhatsHoppin.Forum.Message do
   @doc false
   def changeset(%Message{} = message, attrs) do
     message
-    |> cast(attrs, [:content, :user_id, :brewery_id, :style_id])
-    # |> validate_ids(:brewery_id, :style_id)
-    |> validate_required([:content, :user_id])
+    |> cast(attrs, [:content, :forum_id, :user_id])
+    |> validate_required([:content, :forum_id]) # TODO require user_id, fix migration too
   end
-
-  # TODO get this working
-  # def validate_ids(message, brewery_id, style_id) do
-  #   case valid_ids(brewery_id, style_id) do
-  #     {:ok, _} -> []
-  #     {:error, msg} -> [{field, options[:message] || msg}]
-  #   end
-  # end
-
-  # def valid_ids(brewery_id, style_id) do
-  #   has_brewery? = (brewery_id != -1)
-  #   has_style? = (style_id != -1)
-
-  #   # xor, must have exaxctly one of the two ids
-  #   if (has_brewery? && !has_style?) || (!has_brewery? && has_style?) do
-  #     {:ok, [brewery_id, style_id]}
-  #   else
-  #     {:error, "Must have either a brewery_id or style_id, but not both"}
-  #   end
-  # end
 end
-
-
-
-
-
--- priv/repo/migrations/20171112234804_create_messages.exs
+++ priv/repo/migrations/20171112234804_create_messages.exs
@@ -3,16 +3,13 @@ defmodule WhatsHoppin.Repo.Migrations.CreateMessages do
 
   def change do
     create table(:messages) do
-      add :content, :string
-      add :user_id, references(:users, on_delete: :delete_all), null: false
-      add :brewery_id, references(:breweries, on_delete: :nothing), null: false, default: -1
-      add :style_id, references(:styles, on_delete: :nothing), null: false, default: -1
+      add :content, :string, null: false
+      add :user_id, references(:users, on_delete: :delete_all) #, null: false
+      add :forum_id, :string, null: false
 
       timestamps()
     end
 
     create index(:messages, [:user_id])
-    create index(:messages, [:brewery_id])
-    create index(:messages, [:style_id])
   end
 end
